91ecd56520d47c1ee509ba352c0e76511231949b
Rishi <rishiu@gmail.com>
2020-09-06 14:16:45 -0400

    Adding Array And String ques 4

---
+++ Array And String/04-is_permutation_of_palindrome.py
@@ -0,0 +1,24 @@
+def is_permutation_of_palindrome(sentence):
+  # Initialize a dictionary
+  my_dict = {}
+  # Remove all whitespaces
+  sentence = sentence.replace(" ", "")
+  # iterate over the sentence
+  for letter in sentence:
+    # add all letters in sentence along with their count in my_dict
+    if letter not in my_dict:
+      my_dict[letter] = 0
+    my_dict[letter] += 1
+
+  odd_count = 0
+  # check for no.of letters having odd count in my_dict
+  for i in my_dict.values():
+    if(i%2 == 1): odd_count += 1
+  
+  # if more than two letters have odd count it cant be permutation of a palindrome
+  return (odd_count <=1 )
+
+print(is_permutation_of_palindrome("tact coa"))
+
+
+
